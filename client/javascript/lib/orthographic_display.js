767fe0a1fe2ef50714bf60509fb71d603a205d04
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-05-04 16:39:31 +0300

    mixin any old display, then extend

--- client/javascript/lib/orthographic_display.js
+++ client/javascript/lib/orthographic_display.js
@@ -1,8 +1,7 @@
-define(["underscore", "ext/three", "lib/grid_view", "lib/any_old_display"], function(_, THREE, GridView, AnyOldDisplay) {
+define(["lodash", "ext/three", "lib/grid_view", "lib/any_old_display"], function(_, THREE, GridView, AnyOldDisplay) {
   "use strict";
 
   return function(element, width, height, options) {
-    //TODO: implement as extend
     var setup_camera = function() {
       var camera = new THREE.OrthographicCamera(0, width, 0, height, -2000, 1000);
       camera.position.z = 1;
@@ -17,18 +16,18 @@ define(["underscore", "ext/three", "lib/grid_view", "lib/any_old_display"], func
       return scene;
     };
 
-    var add_orthographic_display_funcs = function(base_display) {
-      base_display.resize = function(width, height) {
+    var display = Object.create(AnyOldDisplay(element, width, height, options)) ;
+    _.extend(display, {
+      camera: setup_camera(),
+      scene: create_a_scene(),
+
+      add_to_scene: function() { _.each(arguments, function(mesh) { this.scene.add(mesh); }.bind(this)); },
+      resize: function(width, height) {
         this.camera.aspect = width / height;
         this.camera.updateProjectionMatrix();
-      };
-      base_display.camera = setup_camera();
-      base_display.scene = create_a_scene();
-      base_display.add_to_scene = function() { _.each(arguments, function(mesh) { this.scene.add(mesh); }.bind(this)); };
+      }
+    });    
 
-      return base_display;
-    };
-
-    return add_orthographic_display_funcs(Object.create(AnyOldDisplay(element, width, height, options)));
+    return display;
   };
 });
