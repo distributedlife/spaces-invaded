c0c4cc8c22bd73ab4ababfed2ef9e59bdef825dd
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-06-14 17:26:13 +0300

    moved generic logic into the framework

--- game/server/js/logic.js
+++ game/server/js/logic.js
@@ -1,5 +1,9 @@
+var rek = require('rekuire');
 var _ = require('underscore');
-var team = require('./enums/teams');
+var team = rek('enums/teams');
+var filter = rek('inch/private/js/common_filters');
+var common = rek('inch/private/js/common_logic');
+var collision_detection = rek('inch/private/js/collision_detection');
 
 module.exports = function(game_state, entities) {
   "use strict";
@@ -8,88 +12,43 @@ module.exports = function(game_state, entities) {
 
   return {
     permanent_things: [game_state.bullet, game_state.tank, game_state.swarm].concat(game_state.invader_bullets).concat(game_state.invaders),
-    temporary_things: [],
 
     all_the_things: function() {
-      return this.permanent_things.concat(this.temporary_things);
+      return this.permanent_things.concat(game_state.fires);
     },
     
     update: function(dt) {
       this.check_for_tank_at_screen_edge();
 
-      //TODO: pull function up to lib/logic but make invocation manual
-      var active_things = _.where(this.all_the_things(), {active: true});
-      //TODO: pull functionup to lib/logic but make invocation manual
-      var things_to_update = _.reject(active_things, function(thing) { return thing.update === undefined; });
+      var active_things = filter.inactive_things(this.all_the_things());
+      var things_to_update = filter.static_things(active_things);
+      var things_that_can_collide = filter.things_that_cant_collide(active_things)
 
-      //TODO: pull functionup to lib/logic but make invocation manual
-      var things_that_can_collide = _.reject(active_things, function(thing) { return thing.collide === undefined; });
-      this.check_for_collisions(things_that_can_collide);
+      collision_detection.check_for_collisions_using_aabb(things_that_can_collide);
+      game_state.wireframes = common.build_wireframes(things_that_can_collide);
 
-      //TODO: move up and only do on conditional
-      game_state.wireframes = _.map(things_that_can_collide, function(thing) {
-        return {
-          id: thing.id,
-          boxes: _.map([].concat(thing.box()), function(box) { return box.dimensions(); }),
-          colliding: thing.colliding || false,
-        };
-      });
-
-      //TODO: pull functionup to lib/logic but make invocation manual
-      _.each(things_to_update, function(thing) { thing.update(dt); });
+      common.update_each_thing(things_to_update, dt);
 
       this.check_for_bullets_off_screen(active_things);
       this.check_for_swarm_at_edge_of_screen();
 
-
-      //TODO: pull functionup to lib/logic but make invocation manual
-      this.expire_temporary_things();
-    },
-
-    //TODO: move to a aabb_collision_detection
-    check_for_collisions: function(things_that_can_collide) {
-      _.each(things_that_can_collide, function(thing) {
-        _.each(things_that_can_collide, function(other_thing) {
-
-          var thing_boxes = [].concat(thing.box());
-          var other_thing_boxes = [].concat(other_thing.box());
-
-          for(var i = 0; i < thing_boxes.length; i++) {
-            for(var j = 0; j < other_thing_boxes.length; j++) {
-
-              if(thing_boxes[i].is_colliding_with(other_thing_boxes[j])) {
-                thing.collide(other_thing);
-                other_thing.collide(thing);
-
-                return;
-              }
-
-            }
-          }
-        });
-      });
-    },
-
-    //TODO: move to game_world_maps_to_screen_size
-    is_off_screen: function(thing) {
-      if (thing.box().bottom() < 0) { return true; }
-      if (thing.box().top() > game_state.dimensions.height) { return true; }
-
-      return false;
+      this.remove_expired_fires();
     },
 
     create_bullet_miss_fire: function (bullet) {
-      var fire = new entities.fire(bullet);
+      game_state.fires.push(new entities.fire(bullet));
+    },
 
-      game_state.fires.push(fire);
-      this.temporary_things.push(fire);
+
+    remove_expired_fires: function() {
+      game_state.fires = filter.active_temporary_things(game_state.fires);
     },
 
     check_for_bullets_off_screen: function(active_things) {
       var bullets = _.filter(active_things, function(thing) { return thing.name === "bullet" || thing.name == "invader bullet"; });
 
       _.each(bullets, function(bullet) {
-        if (this.is_off_screen(bullet)) { 
+        if (common.is_off_screen(bullet, game_state.dimensions)) { 
           bullet.die("out-of-bounds");
           this.create_bullet_miss_fire(bullet);
 
@@ -127,12 +86,6 @@ module.exports = function(game_state, entities) {
           tank.x = game_state.dimensions.width - bb.half_width;
         }
       });
-    },
-
-    //TODO: move to somewhere generic
-    expire_temporary_things: function() {
-      this.temporary_things = _.select(this.temporary_things, function(thing) { return thing.is_alive() === true; });
-      game_state.fires = _.select(game_state.fires, function(thing) { return thing.is_alive() === true; });
     }
   }
 };
